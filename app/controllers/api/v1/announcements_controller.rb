2cae0e822b778b63bf2ec4769cd1dfb4e71a98a3
Victor Jara <Chogo01@hotmail.com>
2021-12-16 10:40:05 -0300

    OT112-84: Index method added
    OT112-84: Improving index controller
    
    OT112-84: params page added

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::AnnouncementsController < ApplicationController
   before_action :authorize_request
+  after_action { pagy_headers_merge(@pagy) if @pagy }
+
+  def index
+    @pagy, @announcement = pagy(Announcement.all, items: 10, page: params[:page])
+    render json: @announcement
+  end
 
   def show
     @announcement = Announcement.find(params[:id])
